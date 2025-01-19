f4ddc87d97dcba6e7d33cbd3205aede1d0cfe642
Arthur Lopes <arthurlopr12@gmail.com>
2025-01-19 16:39:10 -0300

    initial commit

---
+++ src/test/java/com/study/grpcjava/GrpcJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.grpcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
